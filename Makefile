4ac947f417be75a6e577071b943e6f7dbe52d463
James Harrington <james@harrington.net.au>
2019-04-11 19:32:34 -0400

    Update Dockerflie add build scripts and add Makefile

--- Makefile
+++ Makefile
@@ -1,3 +1,56 @@
+BINDIR = bin
+SRC_DIRS = pkg
+GOFILES = $(shell find $(SRC_DIRS) -name '*.go' | grep -v bindata)
+
+BUILD_CMD ?= docker build
+
+DOCKER_CMD ?= docker
+
+# Image URL to use all building/pushing image targets
+IMG ?= certman-operator:latest
+
+# Look up distro name (e.g. Fedora)
+DISTRO ?= $(shell if which lsb_release &> /dev/null; then lsb_release -si; else echo "Unknown"; fi)
+
+OPERATOR_NAME = certman-operator
+
+BINFILE=build/_output/bin/$(OPERATOR_NAME)
+MAINPACKAGE=./cmd/manager
+GOENV=GOOS=linux GOARCH=amd64 CGO_ENABLED=0
+GOFLAGS=-gcflags="all=-trimpath=${GOPATH}" -asmflags="all=-trimpath=${GOPATH}"
+
+TESTTARGETS := $(shell go list -e ./... | egrep -v "/(vendor)/")
+TESTOPTS :=
+
+default: gobuild
+
+.PHONY: clean
+clean:
+	rm -rf ./build/_output
+
+.PHONY: gotest
+gotest:
+	go test $(TESTOPTS) $(TESTTARGETS)
+
+.PHONY: gocheck
+gocheck: ## Lint code
+	gofmt -s -l $(shell go list -f '{{ .Dir }}' ./... ) | grep ".*\.go"; if [ "$$?" = "0" ]; then gofmt -s -d $(shell go list -f '{{ .Dir }}' ./... ); exit 1; fi
+	go vet ./cmd/... ./pkg/...
+
+.PHONY: gobuild
+gobuild: gocheck gotest ## Build binary
+	${GOENV} go build ${GOFLAGS} -o ${BINFILE} ${MAINPACKAGE}
+
+# Build the docker image
+.PHONY: docker-build
+docker-build:
+	$(BUILD_CMD) -t ${IMG} ./build/
+
+# Push the docker image
+.PHONY: docker-push
+docker-push:
+	$(DOCKER_CMD) push ${IMG}
+
 .PHONY: build
 build:
 	go build -o bin/manager github.com/openshift/certman-operator/cmd/manager
---
+++ hack/app_sre_create_image_catalog.sh
@@ -0,0 +1,115 @@
+#!/bin/bash
+
+set -exv
+
+# prefix var with _ so we don't clober the var used during the Make build
+# it probably doesn't matter but we can change it later.
+_OPERATOR_NAME="certman-operator"
+
+BRANCH_CHANNEL="$1"
+QUAY_IMAGE="$2"
+
+GIT_HASH=$(git rev-parse --short=7 HEAD)
+GIT_COMMIT_COUNT=$(git rev-list $(git rev-list --max-parents=0 HEAD)..HEAD --count)
+
+# clone bundle repo
+SAAS_OPERATOR_DIR="saas-${_OPERATOR_NAME}-bundle"
+BUNDLE_DIR="$SAAS_OPERATOR_DIR/${_OPERATOR_NAME}/"
+
+rm -rf "$SAAS_OPERATOR_DIR"
+
+git clone \
+    --branch "$BRANCH_CHANNEL" \
+    https://app:"${APP_SRE_BOT_PUSH_TOKEN}"@github.com/app-sre/saas-${_OPERATOR_NAME}-bundle.git \
+    "$SAAS_OPERATOR_DIR"
+
+# remove any versions more recent than deployed hash
+REMOVED_VERSIONS=""
+if [[ "$REMOVE_UNDEPLOYED" == true ]]; then
+    DEPLOYED_HASH=$(
+        curl -s "https://raw.githubusercontent.com/app-sre/saas-hive/master/${_OPERATOR_NAME}-services/${_OPERATOR_NAME}.yaml" | \
+            docker run --rm -i evns/yq -r '.services[]|select(.name="${_OPERATOR_NAME}").hash'
+    )
+
+    delete=false
+    # Sort based on commit number
+    for version in $(ls $BUNDLE_DIR | sort -t . -k 3 -g); do
+        # skip if not directory
+        [ -d "$BUNDLE_DIR/$version" ] || continue
+
+        if [[ "$delete" == false ]]; then
+            short_hash=$(echo "$version" | cut -d- -f2)
+
+            if [[ "$DEPLOYED_HASH" == "${short_hash}"* ]]; then
+                delete=true
+            fi
+        else
+            rm -rf "${BUNDLE_DIR:?BUNDLE_DIR var not set}/$version"
+            REMOVED_VERSIONS="$version $REMOVED_VERSIONS"
+        fi
+    done
+fi
+
+# generate bundle
+PREV_VERSION=$(ls "$BUNDLE_DIR" | sort -t . -k 3 -g | tail -n 1)
+
+./hack/generate-operator-bundle.py \
+    "$BUNDLE_DIR" \
+    "$PREV_VERSION" \
+    "$GIT_COMMIT_COUNT" \
+    "$GIT_HASH" \
+    "$QUAY_IMAGE:$GIT_HASH"
+
+NEW_VERSION=$(ls "$BUNDLE_DIR" | sort -t . -k 3 -g | tail -n 1)
+
+if [ "$NEW_VERSION" = "$PREV_VERSION" ]; then
+    # stopping script as that version was already built, so no need to rebuild it
+    exit 0
+fi
+
+# create package yaml
+cat <<EOF > $BUNDLE_DIR/${_OPERATOR_NAME}.package.yaml
+packageName: ${_OPERATOR_NAME}
+channels:
+- name: $BRANCH_CHANNEL
+  currentCSV: ${_OPERATOR_NAME}.v${NEW_VERSION}
+EOF
+
+# add, commit & push
+pushd $SAAS_OPERATOR_DIR
+
+git add .
+
+MESSAGE="add version $GIT_COMMIT_COUNT-$GIT_HASH
+
+replaces $PREV_VERSION
+removed versions: $REMOVED_VERSIONS"
+
+git commit -m "$MESSAGE"
+git push origin "$BRANCH_CHANNEL"
+
+popd
+
+# build the registry image
+REGISTRY_IMG="quay.io/app-sre/${_OPERATOR_NAME}-registry"
+DOCKERFILE_REGISTRY="Dockerfile.olm-registry"
+
+cat <<EOF > $DOCKERFILE_REGISTRY
+FROM quay.io/openshift/origin-operator-registry:latest
+
+COPY $SAAS_OPERATOR_DIR manifests
+RUN initializer
+
+CMD ["registry-server", "-t", "/tmp/terminate.log"]
+EOF
+
+docker build -f $DOCKERFILE_REGISTRY --tag "${REGISTRY_IMG}:${BRANCH_CHANNEL}-latest" .
+
+# push image
+skopeo copy --dest-creds "${QUAY_USER}:${QUAY_TOKEN}" \
+    "docker-daemon:${REGISTRY_IMG}:${BRANCH_CHANNEL}-latest" \
+    "docker://${REGISTRY_IMG}:${BRANCH_CHANNEL}-latest"
+
+skopeo copy --dest-creds "${QUAY_USER}:${QUAY_TOKEN}" \
+    "docker-daemon:${REGISTRY_IMG}:${BRANCH_CHANNEL}-latest" \
+    "docker://${REGISTRY_IMG}:${BRANCH_CHANNEL}-${GIT_HASH}"
