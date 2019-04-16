b56ace69cc8eb94743fe2c6f078714f75719cd87
Lisa Seelye <lseelye@redhat.com>
2019-04-16 13:29:36 -0400

    SREP-1143: Prepare for CI pipeline
    Signed-off-by: Lisa Seelye <lseelye@redhat.com>

--- Makefile
+++ Makefile
@@ -1,45 +1,19 @@
-BINDIR = bin
-SRC_DIRS = pkg
-GOFILES = $(shell find $(SRC_DIRS) -name '*.go' | grep -v bindata)
+SHELL := /usr/bin/env bash
 
-BUILD_CMD ?= docker build
+OPERATOR_DOCKERFILE = ./build/Dockerfile
 
-DOCKER_CMD ?= docker
-
-# Image URL to use all building/pushing image targets
-IMG ?= certman-operator:latest
-
-# Look up distro name (e.g. Fedora)
-DISTRO ?= $(shell if which lsb_release &> /dev/null; then lsb_release -si; else echo "Unknown"; fi)
-
-OPERATOR_NAME = certman-operator
-
-BINFILE=build/_output/bin/$(OPERATOR_NAME)
-MAINPACKAGE=./cmd/manager
-GOENV=GOOS=linux GOARCH=amd64 CGO_ENABLED=0
-GOFLAGS=-gcflags="all=-trimpath=${GOPATH}" -asmflags="all=-trimpath=${GOPATH}"
-
-TESTTARGETS := $(shell go list -e ./... | egrep -v "/(vendor)/")
-TESTOPTS :=
+# Include shared Makefiles
+include project.mk
+include standard.mk
 
 default: gobuild
 
-.PHONY: clean
-clean:
-	rm -rf ./build/_output
+# Extend Makefile after here
 
-.PHONY: gotest
-gotest:
-	go test $(TESTOPTS) $(TESTTARGETS)
+BUILD_CMD ?= docker build
 
-.PHONY: gocheck
-gocheck: ## Lint code
-	gofmt -s -l $(shell go list -f '{{ .Dir }}' ./... ) | grep ".*\.go"; if [ "$$?" = "0" ]; then gofmt -s -d $(shell go list -f '{{ .Dir }}' ./... ); exit 1; fi
-	go vet ./cmd/... ./pkg/...
-
-.PHONY: gobuild
-gobuild: gocheck gotest ## Build binary
-	${GOENV} go build ${GOFLAGS} -o ${BINFILE} ${MAINPACKAGE}
+# Image URL to use all building/pushing image targets
+IMG ?= certman-operator:latest
 
 # Build the docker image
 .PHONY: docker-build
@@ -50,11 +24,3 @@ docker-build:
 .PHONY: docker-push
 docker-push:
 	$(BUILD_CMD) -t ${IMG} -f ./build/Dockerfile .
-
-.PHONY: build
-build:
-	go build -o bin/manager github.com/openshift/certman-operator/cmd/manager
-
-.PHONY: test
-test:
-	go test ./pkg/...
---
+++ standard.mk
@@ -0,0 +1,85 @@
+# Validate variables in project.mk exist
+ifndef IMAGE_REGISTRY
+$(error IMAGE_REGISTRY is not set; check project.mk file)
+endif
+ifndef IMAGE_REPOSITORY
+$(error IMAGE_REPOSITORY is not set; check project.mk file)
+endif
+ifndef IMAGE_NAME
+$(error IMAGE_NAME is not set; check project.mk file)
+endif
+ifndef VERSION_MAJOR
+$(error VERSION_MAJOR is not set; check project.mk file)
+endif
+ifndef VERSION_MINOR
+$(error VERSION_MINOR is not set; check project.mk file)
+endif
+
+# Generate version and tag information from inputs
+COMMIT_NUMBER=$(shell git rev-list `git rev-list --parents HEAD | egrep "^[a-f0-9]{40}$$"`..HEAD --count)
+CURRENT_COMMIT=$(shell git rev-parse --short=8 HEAD)
+OPERATOR_VERSION=$(VERSION_MAJOR).$(VERSION_MINOR).$(COMMIT_NUMBER)-$(CURRENT_COMMIT)
+
+OPERATOR_IMAGE_URI=$(IMAGE_REGISTRY)/$(IMAGE_REPOSITORY)/$(IMAGE_NAME):v$(OPERATOR_VERSION)
+OPERATOR_IMAGE_URI_LATEST=$(IMAGE_REGISTRY)/$(IMAGE_REPOSITORY)/$(IMAGE_NAME):latest
+OPERATOR_DOCKERFILE ?=build/ci-operator/Dockerfile
+
+BINFILE=build/_output/bin/$(OPERATOR_NAME)
+MAINPACKAGE=./cmd/manager
+GOENV=GOOS=linux GOARCH=amd64 CGO_ENABLED=0
+GOFLAGS=-gcflags="all=-trimpath=${GOPATH}" -asmflags="all=-trimpath=${GOPATH}"
+
+TESTTARGETS := $(shell go list -e ./... | egrep -v "/(vendor)/")
+# ex, -v
+TESTOPTS := 
+
+ALLOW_DIRTY_CHECKOUT?=false
+
+default: gobuild
+
+.PHONY: clean
+clean:
+	rm -rf ./build/_output
+
+.PHONY: isclean 
+isclean:
+	@(test "$(ALLOW_DIRTY_CHECKOUT)" != "false" || test 0 -eq $$(git status --porcelain | wc -l)) || (echo "Local git checkout is not clean, commit changes and try again." && exit 1)
+
+.PHONY: build
+build: isclean envtest
+	docker build . -f $(OPERATOR_DOCKERFILE) -t $(OPERATOR_IMAGE_URI)
+	docker tag $(OPERATOR_IMAGE_URI) $(OPERATOR_IMAGE_URI_LATEST)
+
+.PHONY: push
+push:
+	docker push $(OPERATOR_IMAGE_URI)
+	docker push $(OPERATOR_IMAGE_URI_LATEST)
+
+.PHONY: gocheck
+gocheck: ## Lint code
+	gofmt -s -l $(shell go list -f '{{ .Dir }}' ./... ) | grep ".*\.go"; if [ "$$?" = "0" ]; then gofmt -s -d $(shell go list -f '{{ .Dir }}' ./... ); exit 1; fi
+	go vet ./cmd/... ./pkg/...
+
+.PHONY: gobuild
+gobuild: gocheck gotest ## Build binary
+	${GOENV} go build ${GOFLAGS} -o ${BINFILE} ${MAINPACKAGE}
+
+.PHONY: gotest
+gotest:
+	go test $(TESTOPTS) $(TESTTARGETS)
+
+.PHONY: envtest
+envtest:
+	@# test that the env target can be evaluated, required by osd-operators-registry
+	@eval $$($(MAKE) env --no-print-directory) || (echo 'Unable to evaulate output of `make env`.  This breaks osd-operators-registry.' && exit 1)
+
+.PHONY: test
+test: envtest gotest
+
+.PHONY: env
+.SILENT: env
+env: isclean
+	echo OPERATOR_NAME=$(OPERATOR_NAME)
+	echo OPERATOR_NAMESPACE=$(OPERATOR_NAMESPACE)
+	echo OPERATOR_VERSION=$(OPERATOR_VERSION)
+	echo OPERATOR_IMAGE_URI=$(OPERATOR_IMAGE_URI)
