c31be61558eb0a28781de64346d564e73a67198b
Carl Brumm <26975605+c-e-brumm@users.noreply.github.com>
2019-08-09 15:44:04 -0700

    Creating an interface and struct for interacting with Let's Encrypt/acme services. Same functionality as the rest of the library.

--- pkg/leclient/lets_encrypt.go
+++ pkg/leclient/lets_encrypt.go
@@ -20,7 +20,7 @@ import (
 	"crypto"
 	"crypto/x509"
 	"encoding/pem"
-	"fmt"
+	//"fmt"
 	"strings"
 
 	"github.com/eggsampler/acme"
@@ -28,6 +28,118 @@ import (
 	"github.com/openshift/certman-operator/config"
 	"sigs.k8s.io/controller-runtime/pkg/client"
 )
+
+type Client interface {
+	GetAccount(client.Client, bool, string) (acme.Account, error)
+	UpdateAccount([]string)
+	CreateOrder([]string)
+	GetOrderURL()
+	OrderAuthorization()
+	FetchAuthorization(string)
+	GetAuthorizationURL()
+	GetAuthorizationIndentifier()
+	SetChallengeType()
+	GetChallengeURL()
+	GetDNS01KeyAuthorization()
+	UpdateChallenge()
+	FinalizeOrder()
+	GetOrderEndpoint()
+	FetchCertificates()
+}
+
+type ACMEClient struct {
+	Client        acme.Client
+	Account       acme.Account
+	Order         acme.Order
+	Authorization acme.Authorization
+	Challenge     acme.Challenge
+}
+
+func (c *ACMEClient) UpdateAccount(certExpiryNotificationList []string) (err error) {
+	c.Account, err = c.Client.UpdateAccount(c.Account, true, certExpiryNotificationList...)
+	return err
+}
+
+func (c *ACMEClient) CreateOrder(domains []string) (err error) {
+	var certDomains []string
+	var ids []acme.Identifier
+
+	for _, domain := range domains {
+		certDomains = append(certDomains, domain)
+		ids = append(ids, acme.Identifier{Type: "dns", Value: domain})
+	}
+	c.Order, err = c.Client.NewOrder(c.Account, ids)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func (c *ACMEClient) GetAccount(kubeClient client.Client, staging bool, namespace string) (err error) {
+	accountURL, err := getLetsEncryptAccountURL(kubeClient, true)
+	if err != nil {
+		return err
+	}
+
+	privateKey, err := getLetsEncryptAccountPrivateKey(kubeClient, true)
+	if err != nil {
+		return err
+	}
+	c.Account = acme.Account{PrivateKey: privateKey, URL: accountURL}
+	return nil
+}
+
+func (c *ACMEClient) GetOrderURL() (URL string, err error) {
+	URL = c.Order.URL
+	return URL, err
+}
+
+func (c *ACMEClient) OrderAuthorization() []string {
+	return c.Order.Authorizations
+}
+
+func (c *ACMEClient) FetchAuthorization(authURL string) (err error) {
+	c.Authorization, err = c.Client.FetchAuthorization(c.Account, authURL)
+	return err
+}
+func (c *ACMEClient) GetAuthorizationURL() string {
+	return c.Authorization.URL
+}
+func (c *ACMEClient) GetAuthorizationIndentifier() string {
+	return c.Authorization.Identifier.Value
+}
+func (c *ACMEClient) SetChallengeType() {
+	c.Challenge = c.Authorization.ChallengeMap["dns-01"]
+}
+func (c *ACMEClient) GetDNS01KeyAuthorization() string {
+	return acme.EncodeDNS01KeyAuthorization(c.Challenge.KeyAuthorization)
+}
+func (c *ACMEClient) GetChallengeURL() string {
+	return c.Challenge.URL
+}
+func (c *ACMEClient) UpdateChallenge() (err error) {
+	c.Challenge, err = c.Client.UpdateChallenge(c.Account, c.Challenge)
+	return err
+}
+func (c *ACMEClient) FinalizeOrder(csr *x509.CertificateRequest) (err error) {
+	c.Order, err = c.Client.FinalizeOrder(c.Account, c.Order, csr)
+	return err
+}
+func (c *ACMEClient) GetOrderEndpoint() string{
+	return c.Order.Certificate
+}
+func (c *ACMEClient) FetchCertificates() (certbundle []*x509.Certificate, err error){
+	certbundle, err = c.Client.FetchCertificates(c.Account, c.Order.Certificate)
+	return certbundle, err
+}
+func NewGetLetsEncryptClient(staging bool) (Client ACMEClient, err error) {
+	if staging {
+		Client.Client, err = acme.NewClient(acme.LetsEncryptStaging)
+		return Client, err
+	}
+	Client.Client, err = acme.NewClient(acme.LetsEncryptProduction)
+	return Client, err
+}
 
 func GetLetsEncryptClient(staging bool) (acme.Client, error) {
 
@@ -111,31 +223,32 @@ func GetCertExpiryNotificationList(email string) []string {
 	return contacts
 }
 
-func EncodeDNS01KeyAuthorization(keyauth string) string {
-	encode := acme.EncodeDNS01KeyAuthorization(keyauth)
-	return encode
-}
-func ChallengeType(auth acme.Authorization) (acme.Challenge, error) {
-	challenge, ok := auth.ChallengeMap["dns-01"]
-	if !ok {
-		var err error
-		return challenge, err
-	}
-	return challenge, nil
-}
+// func EncodeDNS01KeyAuthorization(keyauth string) string {
+// 	encode := acme.EncodeDNS01KeyAuthorization(keyauth)
+// 	return encode
+// }
+// func ChallengeType(auth acme.Authorization) (acme.Challenge, error) {
+// 	//challenge, ok := auth.ChallengeMap["dns-01"]
+// 	challenge, ok := auth.ChallengeMap[acme.ChallengeTypeDNS01]
+// 	if !ok {
+// 		var err error
+// 		return challenge, err
+// 	}
+// 	return challenge, nil
+// }
 
-func CreateOrder(reqLogger logr.Logger, leAccount acme.Account, leClient acme.Client, domains []string) (acme.Order, error) {
-	var certDomains []string
-	var ids []acme.Identifier
+// func CreateOrder(reqLogger logr.Logger, leAccount acme.Account, leClient acme.Client, domains []string) (acme.Order, error) {
+// 	var certDomains []string
+// 	var ids []acme.Identifier
 
-	for _, domain := range domains {
-		reqLogger.Info(fmt.Sprintf("%v domain will be added to certificate request", domain))
-		certDomains = append(certDomains, domain)
-		ids = append(ids, acme.Identifier{Type: "dns", Value: domain})
-	}
-	letsEncryptOrder, err := leClient.NewOrder(leAccount, ids)
-	if err != nil {
-		return letsEncryptOrder, err
-	}
-	return letsEncryptOrder, nil
-}
+// 	for _, domain := range domains {
+// 		reqLogger.Info(fmt.Sprintf("%v domain will be added to certificate request", domain))
+// 		certDomains = append(certDomains, domain)
+// 		ids = append(ids, acme.Identifier{Type: "dns", Value: domain})
+// 	}
+// 	letsEncryptOrder, err := leClient.NewOrder(leAccount, ids)
+// 	if err != nil {
+// 		return letsEncryptOrder, err
+// 	}
+// 	return letsEncryptOrder, nil
+// }
