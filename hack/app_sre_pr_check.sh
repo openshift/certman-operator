c5a72aacff25ed39928bcb52e06509402bbda2fc
Liam Pieri <lpieri@redhat.com>
2019-09-13 10:39:07 -0400

    add yaml validation

--- hack/app_sre_pr_check.sh
+++ hack/app_sre_pr_check.sh
@@ -4,6 +4,13 @@
 
 set -exv
 
+CURRENT_DIR=$(dirname "$0")
+
+python "$CURRENT_DIR"/validate_yaml.py "$CURRENT_DIR"/../deploy/crds
+if [ "$?" != "0" ]; then
+    exit 1
+fi
+
 BASE_IMG="certman-operator"
 IMG="${BASE_IMG}:latest"
 
