8b44ccc00bbe30c8be7943721f6c30a2bd958aad
Stefanie Forrester <sedgar@redhat.com>
2019-09-27 19:05:31 -0700

    backoff is working without any overflow

--- pkg/leclient/utils.go
+++ pkg/leclient/utils.go
@@ -56,9 +56,17 @@ func AddToFailCount(cr *certman.CertificateRequest, queryType string) error {
 	fmt.Println("DEBUG: cr.Status.FailCountAWS")
 	fmt.Println(cr.Status.FailCountAWS)
 	if queryType == "FailCountLetsEncrypt" {
+		if cr.Status.FailCountLetsEncrypt >= 2147483647 {
+			fmt.Println("FailCount not incremented due to overflow possibility")
+			return nil
+		}
 		fmt.Println("DEBUG: Incrementing cr.Status.FailCountLetsEncrypt")
 		cr.Status.FailCountLetsEncrypt = cr.Status.FailCountLetsEncrypt + 1
 	} else if queryType == "FailCountAWS" {
+		if cr.Status.FailCountAWS >= 2147483647 {
+			fmt.Println("FailCount not incremented due to overflow possibility")
+			return nil
+		}
 		fmt.Println("DEBUG: Incrementing cr.Status.FailCountAWS")
 		cr.Status.FailCountAWS = cr.Status.FailCountAWS + 1
 	} else {
--- pkg/sleep/sleep.go
+++ pkg/sleep/sleep.go
@@ -4,18 +4,25 @@
 package sleep
 
 import (
+	"fmt"
 	"math"
 	"time"
 )
 
-// ExponentialBackOff will sleep for a minimum of 2 seconds, maxiumum of 2 hours,
+// ExponentialBackOff will sleep for a minimum of 1 seconds, maxiumum of 4096 seconds,
 // depending on the number of seconds given as failCount (which represents the number
 // of API failures a CertificateRequest has encountered).
 // Sleep time increases by a power of 2 for each API failure.
 // For example, a failCount of 1 sleeps for 2 seconds. A failCount of 2 sleeps for 4 seconds.
 func ExponentialBackOff(failCount int) {
-	// Sleeptime is a minimum of 2 seconds (1<<1), maximum of 2 hours (7200).
-	sleeptime := math.Min(7200, float64(uint(1)<<uint(failCount)))
-	println("Exponential backoff: sleeping", sleeptime, "seconds.")
+
+	fmt.Println("Number of failures encountered: ", failCount)
+	if failCount > 12 {
+		fmt.Println("Resetting failCount to prevent integer overflow and long sleep times")
+		failCount = 12
+	}
+
+	sleeptime := int(math.Exp2(float64(failCount)))
+	fmt.Println("Exponential backoff: sleeping", sleeptime, "seconds.")
 	time.Sleep(time.Duration(sleeptime) * time.Second)
 }
