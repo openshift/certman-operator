f9cb2f0ddce1f1ceb70d56ba034c9934f031e751
Carl Brumm <26975605+c-e-brumm@users.noreply.github.com>
2019-10-16 16:19:53 -0700

    Adding a comment because I'm not bad

--- pkg/controller/certificaterequest/issue_certificate.go
+++ pkg/controller/certificaterequest/issue_certificate.go
@@ -195,6 +195,8 @@ func (r *ReconcileCertificateRequest) IssueCertificate(reqLogger logr.Logger, cr
 
 	reqLogger.Info("certificates are now available")
 
+	// After resolving all new challenges, and storing the cert, delete the challenge records
+	// that were used from dns in this zone.
 	err = r.DeleteAcmeChallengeResourceRecords(reqLogger, cr)
 	if err != nil {
 		reqLogger.Error(err, "error occurred deleting acme challenge resource records from Route53")
