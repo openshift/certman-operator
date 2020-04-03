d3ee7708a16d272e5e9089dce5fbc29943222559
Alex Chvatal <achvatal@redhat.com>
2020-04-03 11:44:16 -0400

    add unit test for RevokeCertificate
    this test confirms that RevokeCertificate returns an error if the
    lets-encrypt account secret isn't present

---
+++ pkg/controller/certificaterequest/revoke_certificate_test.go
@@ -0,0 +1,41 @@
+/*
+Copyright 2020 Red Hat, Inc.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+  
+    http://www.apache.org/licenses/LICENSE-2.0
+  
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package certificaterequest
+
+import (
+  "testing"
+
+  logrTesting "github.com/go-logr/logr/testing"
+)
+
+func TestRevokeCertificate(t *testing.T) {
+  t.Run("errors if lets-encrypt account secret is bad", func(t *testing.T) {
+    testClient := setUpEmptyTestClient(t)
+    rcr := ReconcileCertificateRequest{
+      client: testClient,
+      clientBuilder: setUpFakeAWSClient,
+    }
+
+    nullLogger := logrTesting.NullLogger{}
+
+    err := rcr.RevokeCertificate(nullLogger, certRequest)
+
+    if err == nil {
+      t.Error("expected an error")
+    }
+  })
+}
