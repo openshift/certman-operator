bb69634cd4a979975362f6b1a1e42ee1c75acebf
Dominic Finn <dofinn@redhat.com>
2020-04-08 21:55:33 +1000

    Moved strings to constants

--- pkg/clients/types/types.go
+++ pkg/clients/types/types.go
@@ -1,6 +1,8 @@
 package types
 
 const (
-	AcmeChallengeSubDomain   = "_acme-challenge"
-	WriteValidationSubDomain = "_certman_access_test"
+	AcmeChallengeSubDomain          = "_acme-challenge"
+	WriteValidationSubDomain        = "_certman_access_test"
+	DefaultNotificationEmailAddress = "default_notification_email_address"
+	LetsEncryptEnvironment          = "lets_encrypt_environment"
 )
