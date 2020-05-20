7940ecb0da4ef285782bd0b251e9a0676f21187d
Muhammad Hamza Zaib <hamza.zaib@curity.io>
2020-05-20 12:40:17 +0200

    Fix DNS entry name

--- pkg/clients/azure/dns.go
+++ pkg/clients/azure/dns.go
@@ -21,13 +21,14 @@ import (
 	"encoding/json"
 	"errors"
 	"fmt"
+	"strings"
 
-	"github.com/Azure/azure-sdk-for-go/profiles/latest/dns/mgmt/dns"
-	dnsv1 "github.com/Azure/azure-sdk-for-go/services/dns/mgmt/2018-05-01/dns"
+	"github.com/Azure/azure-sdk-for-go/services/dns/mgmt/2018-05-01/dns"
 	"github.com/Azure/go-autorest/autorest/azure"
 	"github.com/Azure/go-autorest/autorest/azure/auth"
 	"github.com/Azure/go-autorest/autorest/to"
 	"github.com/go-logr/logr"
+
 	corev1 "k8s.io/api/core/v1"
 	"k8s.io/apimachinery/pkg/types"
 	"sigs.k8s.io/controller-runtime/pkg/client"
@@ -43,18 +44,16 @@ const (
 
 // client implements the Client interface
 type azureClient struct {
-	config            auth.ClientCredentialsConfig
-	subscriptionID    string
 	resourceGroupName string
-	recordSetsClient  *dnsv1.RecordSetsClient
-	zonesClient       *dnsv1.ZonesClient
+	recordSetsClient  *dns.RecordSetsClient
+	zonesClient       *dns.ZonesClient
 }
 
 func (c *azureClient) createTxtRecord(reqLogger logr.Logger, recordKey string, recordValue string, zoneName string) (result dns.RecordSet, err error) {
-	recordSetProperties := &dnsv1.RecordSet{
-		RecordSetProperties: &dnsv1.RecordSetProperties{
+	recordSetProperties := &dns.RecordSet{
+		RecordSetProperties: &dns.RecordSetProperties{
 			TTL: to.Int64Ptr(resourceRecordTTL),
-			TxtRecords: &[]dnsv1.TxtRecord{
+			TxtRecords: &[]dns.TxtRecord{
 				{
 					Value: &[]string{
 						recordValue,
@@ -64,42 +63,63 @@ func (c *azureClient) createTxtRecord(reqLogger logr.Logger, recordKey string, r
 		},
 	}
 	reqLogger.Info(fmt.Sprintf("updating hosted zone %v", zoneName))
-	return c.recordSetsClient.CreateOrUpdate(context.TODO(), c.resourceGroupName, zoneName, recordKey, dnsv1.TXT, *recordSetProperties, "", "")
+	return c.recordSetsClient.CreateOrUpdate(context.TODO(), c.resourceGroupName, zoneName, recordKey, dns.TXT, *recordSetProperties, "", "")
+}
+
+func (c *azureClient) generateTxtRecordName(domain string, rootDomain string) string {
+	// Remove base domain
+	domain = strings.TrimSuffix(domain, rootDomain)
+
+	//Remove . at the end if present
+	domain = strings.TrimSuffix(domain, ".")
+
+	//Remove * at the begining if present
+	domain = strings.TrimPrefix(domain, "*")
+
+	//Remove . at the begining if present
+	domain = strings.TrimPrefix(domain, ".")
+
+	return fmt.Sprintf("%s.%s", cTypes.AcmeChallengeSubDomain, domain)
 }
 
 func (c *azureClient) AnswerDNSChallenge(reqLogger logr.Logger, acmeChallengeToken string, domain string, cr *certmanv1alpha1.CertificateRequest) (fqdn string, err error) {
-	fqdn = fmt.Sprintf("%s.%s", cTypes.AcmeChallengeSubDomain, domain)
-	reqLogger.Info(fmt.Sprintf("fqdn acme challenge domain is %v", fqdn))
+	reqLogger.Info(fmt.Sprintf("Cr domain is %v", cr.Spec.ACMEDNSDomain))
+	reqLogger.Info(fmt.Sprintf("Domain is %v", domain))
 
 	zone, err := c.zonesClient.Get(context.TODO(), c.resourceGroupName, cr.Spec.ACMEDNSDomain)
-
 	if err != nil {
 		reqLogger.Error(err, fmt.Sprintf("Error getting dns zone %v", cr.Spec.ACMEDNSDomain))
 		return "", err
 	}
-	_, err = c.createTxtRecord(reqLogger, fqdn, acmeChallengeToken, *zone.Name)
 
+	txtRecordName := c.generateTxtRecordName(domain, *zone.Name)
+	recordSet, err := c.createTxtRecord(reqLogger, txtRecordName, acmeChallengeToken, *zone.Name)
+
+	loc := (*(*recordSet.TxtRecords)[0].Value)[0]
+	reqLogger.Info(fmt.Sprintf("record set added: %v", loc))
 	if err != nil {
 		reqLogger.Error(err, "Error adding acme challenge DNS entry")
 		return "", err
 	}
-	return fqdn, nil
+	return txtRecordName + "." + cr.Spec.ACMEDNSDomain, nil
 }
 
 func (c *azureClient) DeleteAcmeChallengeResourceRecords(reqLogger logr.Logger, cr *certmanv1alpha1.CertificateRequest) error {
-	fqdn := fmt.Sprintf("%s.%s", cTypes.AcmeChallengeSubDomain, cr.Spec.ACMEDNSDomain)
-
 	zone, err := c.zonesClient.Get(context.TODO(), c.resourceGroupName, cr.Spec.ACMEDNSDomain)
 	if err != nil {
 		reqLogger.Error(err, fmt.Sprintf("Error getting dns zone %v", cr.Spec.ACMEDNSDomain))
 		return err
 	}
 
-	_, err = c.recordSetsClient.Delete(context.TODO(), c.resourceGroupName, *zone.Name, fqdn, dnsv1.TXT, "")
+	for _, dnsName := range cr.Spec.DnsNames {
+		txtRecordName := c.generateTxtRecordName(dnsName, cr.Spec.ACMEDNSDomain)
+		reqLogger.Info(fmt.Sprintf("Deleting record set %v", txtRecordName))
+		_, err = c.recordSetsClient.Delete(context.TODO(), c.resourceGroupName, *zone.Name, txtRecordName, dns.TXT, "")
 
-	if err != nil {
-		reqLogger.Error(err, "Error deleting acme challenge DNS entry")
-		return err
+		if err != nil {
+			reqLogger.Error(err, "Error deleting acme challenge DNS entry")
+			return err
+		}
 	}
 
 	return nil
@@ -127,7 +147,7 @@ func (c *azureClient) ValidateDNSWriteAccess(reqLogger logr.Logger, cr *certmanv
 		}
 
 		// After successfull write test clean up the test record and test deletion of that record.
-		_, err = c.recordSetsClient.Delete(context.TODO(), c.resourceGroupName, *zone.Name, recordKey, dnsv1.TXT, "")
+		_, err = c.recordSetsClient.Delete(context.TODO(), c.resourceGroupName, *zone.Name, recordKey, dns.TXT, "")
 
 		if err != nil {
 			reqLogger.Error(err, "Error while deleting Write Access record")
@@ -195,9 +215,7 @@ func NewClient(kubeClient client.Client, secretName string, namespace string, re
 	zonesClient.Authorizer = authorizer
 
 	return &azureClient{
-		config:            config,
 		resourceGroupName: resourceGroupName,
-		subscriptionID:    subscriptionID,
 		recordSetsClient:  &recordSetsClient,
 		zonesClient:       &zonesClient,
 	}, nil
