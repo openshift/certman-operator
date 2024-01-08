4be2dc3737cbdd700dbc26474f6aa86c461d20ec
Trevor Nierman <tnierman@redhat.com>
2024-01-08 14:26:52 -0600

    Boilerplate: Update to a48d90919cb9a4fe3b4bbd51ac6ef71ea1b49ed6
    Conventions:
    - openshift/golang-osd-operator: Update
    ---
    https://github.com/openshift/boilerplate/compare/f4f0c62f44c1a2547a5c0c4b1a5611cb6bde23e5...a48d90919cb9a4fe3b4bbd51ac6ef71ea1b49ed6
    
    commit: c93835dd34f897e62b1ef5a88e1640fb9774850b
    author: Supreeth Basabattini
    fix: Invalid option reference
    
    commit: ece25ada17378a38dff53847d7ed21132c0caf6d
    author: Supreeth Basabattini
    TOC added
    
    commit: 75e862e6b6fc86ca7df41fcc9612929247cd8d6d
    author: Supreeth Basabattini
    Docs added + cleanup
    
    commit: ece978220f0ccdbb00b45f703a2cec01d32d1448
    author: Supreeth Basabattini
    feat: introduce OLM Skiprange
    
    commit: cc80888d2bee2552168a9a285d27656e13c5fae5
    author: Ritu Mundhe
    Added env var to osde2e pod template
    
    commit: 5d593fd53f826d2886e2086b9adba67caed868ac
    author: Supreeth Basabattini
    Make image digest generation modular
    
    Removed redundant lines
    
    commit: e124c758f491297ef6d9f273296875786bf12008
    author: Supreeth Basabattini
    feat: Support multiple deployments

--- boilerplate/openshift/golang-osd-operator/csv-generate/csv-generate.sh
+++ boilerplate/openshift/golang-osd-operator/csv-generate/csv-generate.sh
@@ -4,10 +4,10 @@ set -e
 
 source `dirname $0`/common.sh
 
-usage() { echo "Usage: $0 -o operator-name -c saas-repository-channel -H operator-commit-hash -n operator-commit-number -i operator-image -V operator-version" 1>&2; exit 1; }
+usage() { echo "Usage: $0 -o operator-name -c saas-repository-channel -H operator-commit-hash -n operator-commit-number -i operator-image -V operator-version -s supplementary-image -e skip-range-enabled" 1>&2; exit 1; }
 
 # TODO : Add support of long-options
-while getopts "c:dg:H:i:n:o:V:" option; do
+while getopts "c:dg:H:i:n:o:V:s:e:" option; do
     case "${option}" in
         c)
             operator_channel=${OPTARG}
@@ -32,6 +32,12 @@ while getopts "c:dg:H:i:n:o:V:" option; do
             # Notably, it does *not* start with `v`.
             operator_version=${OPTARG}
             ;;
+        s)
+            supplementary_image=${OPTARG}
+            ;;
+        e)
+            skip_range_enabled=${OPTARG}
+            ;;
         *)
             usage
     esac
@@ -53,13 +59,16 @@ else
     YQ_CMD="$CONTAINER_ENGINE run --rm -i $yq_image"
 fi
 
-# Get the image URI as repo URL + image digest
-IMAGE_DIGEST=$(skopeo inspect docker://${operator_image}:v${operator_version} | jq -r .Digest)
-if [[ -z "$IMAGE_DIGEST" ]]; then
-    echo "Couldn't discover IMAGE_DIGEST for docker://${operator_image}:v${operator_version}!"
-    exit 1
+REPO_DIGEST=$(generateImageDigest $operator_image $operator_version)
+
+# Given a supplementary image is specified,
+# generate the image digest.
+if [[ -n $supplementary_image ]]; then
+    SECONDARY_REPO_DIGEST=$(generateImageDigest $supplementary_image $operator_version)
+    SECONDARY_REPO_DIGEST="-s ${SECONDARY_REPO_DIGEST}"
+else
+    SECONDARY_REPO_DIGEST=""
 fi
-REPO_DIGEST=${operator_image}@${IMAGE_DIGEST}
 
 # If no override, using the gitlab repo
 if [ -z "$GIT_PATH" ] ; then
@@ -76,7 +85,7 @@ git clone --branch "$operator_channel" ${GIT_PATH} "$SAAS_OPERATOR_DIR"
 # If this is a brand new SaaS setup, then set up accordingly
 if [[ ! -d "${BUNDLE_DIR}" ]]; then
     echo "Setting up new SaaS operator dir: ${BUNDLE_DIR}"
-    mkdir "${BUNDLE_DIR}"
+    mkdir -p "${BUNDLE_DIR}"
 fi
 
 # For testing purposes, support disabling anything that relies on
@@ -175,12 +184,12 @@ fi
 # ...Unless we're already in a container, which is how boilerplate
 # CI runs. We have py3 there, so run natively in that case.
 if [[ -z "$CONTAINER_ENGINE" ]]; then
-    ./boilerplate/openshift/golang-osd-operator/csv-generate/common-generate-operator-bundle.py -o ${operator_name} -d ${OUTPUT_DIR} ${PREV_VERSION_OPTS} -i ${REPO_DIGEST} -V ${operator_version}
+    ./boilerplate/openshift/golang-osd-operator/csv-generate/common-generate-operator-bundle.py -o ${operator_name} -d ${OUTPUT_DIR} ${PREV_VERSION_OPTS} -i ${REPO_DIGEST} -V ${operator_version} ${SECONDARY_REPO_DIGEST} -e ${skip_range_enabled}
 else
     if [[ ${CONTAINER_ENGINE##*/} == "podman" ]]; then
         CE_OPTS="--userns keep-id -v `pwd`:`pwd`:Z"
     else
         CE_OPTS="-v `pwd`:`pwd`"
     fi
-    $CONTAINER_ENGINE run --pull=always --rm ${CE_OPTS} -u `id -u`:0 -w `pwd` registry.access.redhat.com/ubi8/python-36 /bin/bash -c "python -m pip install --disable-pip-version-check oyaml; python ./boilerplate/openshift/golang-osd-operator/csv-generate/common-generate-operator-bundle.py -o ${operator_name} -d ${OUTPUT_DIR} ${PREV_VERSION_OPTS} -i ${REPO_DIGEST} -V ${operator_version}"
+    $CONTAINER_ENGINE run --pull=always --rm ${CE_OPTS} -u `id -u`:0 -w `pwd` registry.access.redhat.com/ubi8/python-36 /bin/bash -c "python -m pip install --disable-pip-version-check oyaml; python ./boilerplate/openshift/golang-osd-operator/csv-generate/common-generate-operator-bundle.py -o ${operator_name} -d ${OUTPUT_DIR} ${PREV_VERSION_OPTS} -i ${REPO_DIGEST} -V ${operator_version} ${SECONDARY_REPO_DIGEST} -e ${skip_range_enabled}"
 fi
