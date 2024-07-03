10c09d44166ff14133bd48a405f2980d9ae1435a
Yiqin Zhang <yiqzhang@redhat.com>
2024-07-03 11:35:33 -0400

    git reset b5cb884

--- osde2e/certman_operator_tests.go
+++ osde2e/certman_operator_tests.go
@@ -6,16 +6,60 @@ package osde2etests
 
 import (
 	"context"
+	"time"
+
 	. "github.com/onsi/ginkgo/v2"
+	. "github.com/onsi/gomega"
+	configv1 "github.com/openshift/client-go/config/clientset/versioned/typed/config/v1"
+	"github.com/openshift/osde2e-common/pkg/clients/openshift"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/client-go/kubernetes"
+	"sigs.k8s.io/controller-runtime/pkg/log"
 )
 
 var _ = Describe("Certman Operator", Ordered, func() {
+	var (
+		k8s        *openshift.Client
+		clientset  *kubernetes.Clientset
+		secretName string
+	)
+	const (
+		pollingDuration = 15 * time.Minute
+		namespace       = "openshift-config"
+	)
+
 	BeforeAll(func(ctx context.Context) {
+		log.SetLogger(GinkgoLogr)
+		var err error
+		k8s, err = openshift.New(GinkgoLogr)
+		Expect(err).ShouldNot(HaveOccurred(), "Unable to setup k8s client")
+		clientset, err = kubernetes.NewForConfig(k8s.GetConfig())
+		Expect(err).ShouldNot(HaveOccurred(), "Unable to setup clientset")
 	})
 
 	It("certificate secret exists under openshift-config namespace", func(ctx context.Context) {
+		Eventually(func() bool {
+			listOpts := metav1.ListOptions{LabelSelector: "certificate_request"}
+			secrets, err := clientset.CoreV1().Secrets(namespace).List(ctx, listOpts)
+			if err != nil || len(secrets.Items) < 1 {
+				return false
+			}
+			secretName = secrets.Items[0].Name
+			return true
+		}, pollingDuration, 30*time.Second).Should(BeTrue(), "Certificate secret should exist under openshift-config namespace")
 	})
 
 	It("certificate secret should be applied to apiserver object", func(ctx context.Context) {
+		Eventually(func() bool {
+			configClient, err := configv1.NewForConfig(k8s.GetConfig())
+			if err != nil {
+				return false
+			}
+			apiserver, err := configClient.APIServers().Get(ctx, "cluster", metav1.GetOptions{})
+			if err != nil || len(apiserver.Spec.ServingCerts.NamedCertificates) < 1 {
+				return false
+			}
+			return apiserver.Spec.ServingCerts.NamedCertificates[0].ServingCertificate.Name == secretName
+		}, pollingDuration, 30*time.Second).Should(BeTrue(), "Certificate secret should be applied to apiserver object")
 	})
 })
