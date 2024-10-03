46da31dc974fdb22ef819f1f0794067ba81253de
ritmun <rmundhe@redhat.com>
2024-10-03 14:52:56 -0500

    reconciler can't be defined in both WithOptions and Complete
    remove it from WithOptions, keep it in Complete

--- controllers/clusterdeployment/clusterdeployment_controller.go
+++ controllers/clusterdeployment/clusterdeployment_controller.go
@@ -34,7 +34,6 @@ import (
 
 	ctrl "sigs.k8s.io/controller-runtime"
 	"sigs.k8s.io/controller-runtime/pkg/client"
-	"sigs.k8s.io/controller-runtime/pkg/controller"
 	"sigs.k8s.io/controller-runtime/pkg/controller/controllerutil"
 	logf "sigs.k8s.io/controller-runtime/pkg/log"
 	"sigs.k8s.io/controller-runtime/pkg/reconcile"
@@ -426,8 +425,5 @@ func (r *ClusterDeploymentReconciler) SetupWithManager(mgr ctrl.Manager) error {
 	return ctrl.NewControllerManagedBy(mgr).
 		For(&hivev1.ClusterDeployment{}).
 		Owns(&certmanv1alpha1.CertificateRequest{}).
-		WithOptions(controller.Options{
-			Reconciler: r,
-		}).
 		Complete(r)
 }
