ed7048a1e4686396bad7aa7797d2db88d7290fc4
Karanjit Singh <karanjsi@redhat.com>
2025-08-06 03:09:36 -0400

    SREP-1215: Initial commit for delete primary cert bundle if exists

--- test/e2e/certman_operator_tests.go
+++ test/e2e/certman_operator_tests.go
@@ -6,12 +6,14 @@ package osde2etests
 
 import (
 	"context"
+	"fmt"
 	"time"
 
 	. "github.com/onsi/ginkgo/v2"
 	. "github.com/onsi/gomega"
 	configv1 "github.com/openshift/client-go/config/clientset/versioned/typed/config/v1"
 	"github.com/openshift/osde2e-common/pkg/clients/openshift"
+	"k8s.io/apimachinery/pkg/api/errors"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/client-go/kubernetes"
 	"sigs.k8s.io/controller-runtime/pkg/log"
@@ -24,8 +26,9 @@ var _ = Describe("Certman Operator", Ordered, func() {
 		secretName string
 	)
 	const (
-		pollingDuration = 15 * time.Minute
-		namespace       = "openshift-config"
+		pollingDuration            = 15 * time.Minute
+		namespace                  = "openshift-config"
+		namespace_certman_operator = "certman-operator"
 	)
 
 	BeforeAll(func(ctx context.Context) {
@@ -62,4 +65,33 @@ var _ = Describe("Certman Operator", Ordered, func() {
 			return apiserver.Spec.ServingCerts.NamedCertificates[0].ServingCertificate.Name == secretName
 		}, pollingDuration, 30*time.Second).Should(BeTrue(), "Certificate secret should be applied to apiserver object")
 	})
+
+	It("delete secret, primary-cert-bundle-secret, if exists", func(ctx context.Context) {
+		secretNameToDelete := "primary-cert-bundle-secret"
+		pollingDuration := 5 * time.Minute
+		pollInterval := 30 * time.Second
+
+		originalSecret, err := clientset.CoreV1().Secrets(namespace_certman_operator).Get(ctx, secretNameToDelete, metav1.GetOptions{})
+		if errors.IsNotFound(err) {
+			log.Log.Info("Secret does not exist, skipping deletion test.")
+			return
+		}
+		Expect(err).ShouldNot(HaveOccurred(), "Error retrieving the original secret")
+
+		originalTimestamp := originalSecret.CreationTimestamp.Time
+		log.Log.Info(fmt.Sprintf("Original secret creation timestamp: %v", originalTimestamp))
+
+		err = clientset.CoreV1().Secrets(namespace_certman_operator).Delete(ctx, secretNameToDelete, metav1.DeleteOptions{})
+		Expect(err).ShouldNot(HaveOccurred(), "Failed to delete the secret")
+
+		Eventually(func() bool {
+			newSecret, err := clientset.CoreV1().Secrets(namespace_certman_operator).Get(ctx, secretNameToDelete, metav1.GetOptions{})
+			if err != nil {
+				return false
+			}
+			return newSecret.CreationTimestamp.Time.After(originalTimestamp)
+		}, pollingDuration, pollInterval).Should(BeTrue(),
+			fmt.Sprintf("Secret %q was not re-created within %v or timestamp did not change", secretNameToDelete, pollingDuration))
+	})
+
 })
